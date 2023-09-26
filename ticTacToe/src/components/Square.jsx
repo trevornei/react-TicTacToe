572d4102cbb0c822fa63409b938fce809866f46e
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-26 17:49:37 -0600

    Added the handleClick funtion to Square.jsx. Each button now has an onClick that calls handleClick.

--- ticTacToe/src/components/Square.jsx
+++ ticTacToe/src/components/Square.jsx
@@ -3,11 +3,18 @@ import React from "react";
 // Now pass down { value } as a prop.
 // Then pass value into the div by escaping into JSX
 
+// Inside of Square: We need a way to handle the click event.
+// create a function called handleClick() {}
+// now place handle click inside the button element.
+
 export default function Square ({ value }) {
+    function handleClick() {
+        console.log("Clicked : D")
+    }
     return (
         <>
             <div className="square">
-                <button>{value}</button>
+                <button onClick={handleClick}>{value}</button>
             </div>
         </>
     )
