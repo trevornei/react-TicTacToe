a1585eb1f01bf79a7b30e3ad0254f6c14d509a6e
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-26 17:59:21 -0600

    Added useState() fn to Square.jsx and removed value={value} from board.jsx.

--- ticTacToe/src/components/Board.jsx
+++ ticTacToe/src/components/Board.jsx
@@ -16,19 +16,19 @@ export default function board() {
         <>
             <div className="mt-4 mb-12 p-4 rounded-sm bg-gradient-to-bl from-violet-800 to-cyan-500">
                 <div className="board-row">
-                        <Square value={1}/>
-                        <Square value={2}/>
-                        <Square value={3}/>
+                        <Square/>
+                        <Square/>
+                        <Square/>
                 </div>
                 <div className="board-row">
-                    <Square value={4}/>
-                    <Square value={5}/>
-                    <Square value={6}/>
+                    <Square/>
+                    <Square/>
+                    <Square/>
                 </div>
                 <div className="board-row">
-                    <Square value={7}/>
-                    <Square value={8}/>
-                    <Square value={9}/>
+                    <Square/>
+                    <Square/>
+                    <Square/>
                 </div>
             </div>
         </>
--- ticTacToe/src/components/Square.jsx
+++ ticTacToe/src/components/Square.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 // Now pass down { value } as a prop.
 // Then pass value into the div by escaping into JSX
@@ -7,7 +8,16 @@ import React from "react";
 // create a function called handleClick() {}
 // now place handle click inside the button element.
 
+// Now we need to import state.
+// Above the handleClick() call the useState() fn.
+// [value, setValue] 
+// value will store the value. 
+// setValue is a function that changes the value. 
+
+
 export default function Square ({ value }) {
+    const [value, setValue] = useState(null)
+
     function handleClick() {
         console.log(`Clicked square number: ${value} :D`)
     }
