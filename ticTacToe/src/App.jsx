a8a6df06b5f2024343c2cc6fb62ac8c086f1dab4
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-26 12:39:33 -0600

    copy and pasted the styles for index.css for the tutorial.

--- ticTacToe/src/App.jsx
+++ ticTacToe/src/App.jsx
@@ -1,2 +1,10 @@
-// Create a functio
+// Create a function called square and add export default statement. 
+// Square() should return a button with "x" as the text
 
+export default function Square() {
+    return (
+        <div className="">
+            <button className="square">X</button>
+        </div>
+    )
+}
