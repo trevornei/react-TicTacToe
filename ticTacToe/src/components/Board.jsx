859d09dc21580e85f3704f12c40c793cf9cdc2c1
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-27 17:08:26 -0600

    Added winning condition logic.

--- ticTacToe/src/components/Board.jsx
+++ ticTacToe/src/components/Board.jsx
@@ -22,14 +22,43 @@ export default function board() {
     const [squares, setSquares] = useState(Array(9).fill(null))
     const [xIsNext, setXIsNext] = useState(true)
 
+    /*
+        Validate Winner: Make a function called isWinner.
+        --> Pass in the Argument squares
+        --> Inside Fn: create an array of array's called lines.
+        --> each array stores the values for the winning sequences
+    */
+
+    function isWinnner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+          ]
+        // For loop iterates over the inner array
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i]
+            // if statement checks if x === x || o === o;
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]  ) {
+                return squares[a]
+            }
+        }
+        return null;
+    }
+
     function handleClick(i) {
-        {/*
+        /*
             We need to factor in that for each click, we need to take account for the index that the click is taking place in.  
             The variable nextSquares creates a copy of the squares array the .slice() method. 
                 -> This is important because we are able to replace the data with a new copy with the altered data.
                 -> The goal here is to NOT mutate the squares array, rather make a copy.
                 -> The 'time travel' feature of the app is dependent on the original array remaining intact and altering the data in a copied array.
-        */}
+        */
 
         /*
             Preventing squares from overriding the first click:
@@ -44,7 +73,7 @@ export default function board() {
             Regardless, I learned that if condition is truthy, then using the keyword return will abort the function.
         */
 
-        if (squares[i]) {
+        if (squares[i] || isWinnner(squares)) {
             return
         }
 
@@ -57,6 +86,14 @@ export default function board() {
         }
         setSquares(nextSquares)
         setXIsNext(!xIsNext)
+    }
+
+    const winner = isWinnner(squares)
+    let status
+    if (winner) {
+        status = `${winner} won the game!`
+    } else {
+        status = `Next ${xIsNext ? 'X' : 'O'}`
     }
 
     console.log(squares)
@@ -81,6 +118,9 @@ export default function board() {
                     <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
                     <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
                 </div>
+            </div>
+            <div className="status">
+                {status}
             </div>
         </>
     )
